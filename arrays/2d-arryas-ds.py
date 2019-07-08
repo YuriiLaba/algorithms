7b9512d10966594f4bd417a1690dbd93477eee21
laba <laba@ws-lv-cp3117.eleks-software.local>
2019-07-08 13:45:26 +0300

    initial commit

---
+++ arrays/2d-arryas-ds.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the hourglassSum function below.
+def hourglassSum(arr):
+    pass
+
+if __name__ == '__main__':
+    #fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr = []
+
+    for x in range(10):
+        print(random.randint(1,101))
+
+    #for _ in range(6):
+    #    arr.append(list(map(int, input().rstrip().split())))
+
+    #print(arr)
+
+    # result = hourglassSum(arr)
+
+    #fptr.write(str(result) + '\n')
+
+    #fptr.close()
