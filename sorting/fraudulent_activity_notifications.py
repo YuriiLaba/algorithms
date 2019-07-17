5cff3f81ae18d9756aff7808e28793d2d94bb825
YuriyVilnitskiy <laba@ws-lv-cp3117.eleks-software.local>
2019-07-17 19:25:32 +0300

    updated

--- sorting/fraudulent_activity_notifications.py
+++ sorting/fraudulent_activity_notifications.py
@@ -8,40 +8,29 @@ import bisect
 
 
 def _binarySearch(arr, el, left_idx, right_idx):
-    print(left_idx, right_idx)
-    mid = left_idx + int((right_idx - left_idx) // 2)
+    mid = left_idx + int((right_idx - left_idx) // 2) - ((right_idx - left_idx) % 2 > 0)
 
-    print(mid)
-    # print(arr[mid])
-    if arr[mid-1] == el:
+    if left_idx == right_idx:
+        return
+    if arr[mid] == el:
         return mid
-    elif arr[mid] > el:
-        # print(arr[mid], el)
+    elif arr[mid] < el:
         return _binarySearch(arr, el, mid, right_idx)
-    elif arr[mid] < el: 
+    elif arr[mid] > el: 
         return _binarySearch(arr, el, left_idx, mid)
-    
-    # if right_idx >= left_idx: 
-  
-        # mid = int((right_idx - left_idx) / 2) + ((right_idx - left_idx) % 2 > 0)
-        # print(mid)
-        # If element is present at the middle itself 
-        # if arr[mid] == el: 
-            # return mid 
-          
-        # If element is smaller than mid, then it  
-        # can only be present in left subarray 
-        # elif arr[mid] > el: 
-            # return _binarySearch(arr, el, left_idx, mid) 
-  
-        # Else the element can only be present  
-        # in right subarray 
-        # else: 
-            # return _binarySearch(arr, el, mid, right_idx) 
-  
-    # else: 
-        # Element is not present in the array 
-        # return -1
+
+def _binaryInsert(arr, el, left_idx, right_idx):
+    mid = left_idx + int((right_idx - left_idx) / 2) - ((right_idx - left_idx) % 2 > 0)
+
+
+    if left_idx == right_idx:
+        return
+    if  arr[mid-1] <= el <= arr[mid]:
+        return mid
+    elif arr[mid] < el:
+        return _binaryInsert(arr, el, mid, right_idx)
+    elif arr[mid] > el: 
+        return _binaryInsert(arr, el, left_idx, mid)
 
 
 
@@ -70,33 +59,41 @@ def activityNotifications(expenditure, d):
 
 
             a = _binarySearch(expenditure_portion, expenditure[start_day], 0, len(expenditure_portion))
-            j = bisect.bisect_left(expenditure_portion, expenditure[start_day])
-            print(a, j)
-            del expenditure_portion[j]
+            del expenditure_portion[a]
 
-            bisect.insort(expenditure_portion, expenditure[last_day])
+            # bisect.insort(expenditure_portion, expenditure[last_day])
+            if expenditure[last_day] >= expenditure_portion[-1]:
+                expenditure_portion.insert(-1, expenditure[last_day])
+            elif expenditure[last_day] <= expenditure_portion[0]:
+                expenditure_portion.insert(0, expenditure[last_day])
+            else:
+                idx = _binaryInsert(expenditure_portion, expenditure[last_day], 0, len(expenditure_portion))
+                expenditure_portion.insert(idx, expenditure[last_day])
+            
+            # expenditure_portion.append(expenditure[last_day])
             start_day += 1
             last_day += 1
 
         return num_notif
 
-
 if __name__ == '__main__':
 
-    d = 40001
+    d = 3
 
     with open("/home/laba/Project/hacker-rank/sorting/40001.txt") as f:
         lines = f.read().split(' ')
-    expenditure = list(map(int, lines))
-    if(7 in sorted(expenditure)):
-        print("DDD")
-    _binarySearch(sorted(expenditure), 7, 0, len(expenditure))
+    # expenditure = list(map(int, lines))
+    expenditure = [2, 3, 4, 2, 3, 6, 8, 4, 5]
+    # if(7 in sorted(expenditure)):
+        # print("DDD")
+    # d = _binarySearch(sorted(expenditure), 7, 0, len(expenditure))
+    # print(d)
     # start = time.time()
-    # result = activityNotifications(expenditure, d)
+    result = activityNotifications(expenditure, d)
     # end = time.time()
 
     # print(end - start)
-    # print(result)
+    print(result)
     # lst = [ 2, 3, 6, 7, 8, 9, 10]
     # _binarySearch(lst, 10, 0, len(lst))
     # print(lst[a])
